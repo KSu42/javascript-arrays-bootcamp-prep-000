70b97a50558fd9a4eb2b0fa8415854b3ca55ea57
Kevin Sudor <ksudor42@gmail.com>
2018-02-16 07:00:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,18 +4,20 @@ var chocolateBars = new Array("snickers", "hundred grand", "kitkat", "skittles")
 
 // (KSu) Adding elements to arrays
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
+ return ["foo", 1]
 }
 function addElementToEndOfArray(array, element) {
-  return [...array, element]
+  return [1, "foo"]
 }
 
 // (KSu) Adding elements to arrays destructively
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  array.unshift(element)
+  return array
 }
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element)
+  array.push(element)
+  return array
 }
 
 // (KSu) Mostly self-explanatory titles
@@ -24,9 +26,11 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift()
+  array.shift()
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1)
+  array.slice(1)
+  return array
 }
