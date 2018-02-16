3eaaa1043c8037de93eb235e659b097f220607ba
Kevin Sudor <ksudor42@gmail.com>
2018-02-16 07:05:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,13 @@
 // (KSu) Defining a variable that has a value of an array of strings
 var chocolateBars = new Array("snickers", "hundred grand", "kitkat", "skittles")
 
-
+// (KSu) Adding elements to arrays
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
 
 // (KSu) Adding elements to arrays destructively
 function destructivelyAddElementToBeginningOfArray(array, element) {
