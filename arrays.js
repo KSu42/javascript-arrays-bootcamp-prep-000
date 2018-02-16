c52ce3ae5255a5242405effaaa68dfcfc2d7751c
Kevin Sudor <ksudor42@gmail.com>
2018-02-16 06:43:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
+  var array = [1, 2, 3]
   array.slice(1)
   return array
 }
